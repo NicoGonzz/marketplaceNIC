0f5def43894bffa5de00186cf6c2c349477b8013
NicoGonzz <juannicolas28@live.com>
2024-11-05 13:35:29 -0500

    Structure all CRUDS and product-clients components

--- src/app/admin/admin-login/admin-login.component.ts
+++ src/app/admin/admin-login/admin-login.component.ts
@@ -1,12 +1,40 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-admin-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule,CommonModule],
   templateUrl: './admin-login.component.html',
   styleUrl: './admin-login.component.css'
 })
 export class AdminLoginComponent {
+  loginForm: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]], 
+      password: ['', [Validators.required, Validators.minLength(6)]] 
+    });
+  }
+
+  get email() {
+    return this.loginForm.get('email');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
+  }
+
+  login() {
+    if (this.loginForm.valid) {
+      const user = this.loginForm.value;
+      console.log('User:', user);
+      // Aquí puedes agregar la lógica para autenticar al usuario
+    } else {
+      console.log('Formulario no válido');
+    }
+  }
 }
+
--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,12 +1,34 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+
+interface Product {
+  id: number;
+  name: string;
+  price: number;
+  purchaseTime: string; 
+  client: string; 
+}
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product.component.html',
-  styleUrl: './product.component.css'
+  styleUrls: ['./product.component.css']
 })
 export class ProductComponent {
+  products: Product[] = [
+    { id: 1, name: 'Laptop', price: 1200, purchaseTime: '2024-11-04T10:00:00', client: 'John Doe' },
+    { id: 2, name: 'Smartphone', price: 800, purchaseTime: '2024-11-04T11:30:00', client: 'Jane Smith' },
+    { id: 3, name: 'Headphones', price: 150, purchaseTime: '2024-11-04T12:15:00', client: 'Alice Johnson' },
+    { id: 4, name: 'Camera', price: 600, purchaseTime: '2024-11-04T13:45:00', client: 'Bob Brown' },
+    { id: 5, name: 'Smartwatch', price: 200, purchaseTime: '2024-11-04T14:30:00', client: 'Charlie White' }
+  ];
 
+  processPayment(productId: number) {
+    const product = this.products.find(p => p.id === productId);
+    if (product) {
+      console.log(`Processing payment for ${product.name}...`);
+    }
+  }
 }
